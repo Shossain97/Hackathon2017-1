5c869189977ed51539bc4606c0b5229cfba822c6
colby <colby@LAPTOP-G79CEIBK>
2017-02-18 14:05:14 -0600

    This won't work.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,10 +1,27 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.*;
 
 public class MainWindow 
-{
+{	
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello fucker, mother world.");
-		System.out.println("We have anal beads");
-		System.out.println("Nooooo fuckery.");
+		// Set up the window frame.
+		JFrame frame = new JFrame("Dropping Logic");
+		frame.setSize(1080, 920);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Main panel container.
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		
+		DragPanels dragPanels = new DragPanels(frame);
+		
+		mainPanel.add(dragPanels.GetComponentPanel(), BorderLayout.WEST);
+		mainPanel.add(dragPanels.GetLogicPanel(), BorderLayout.EAST);
+		
+		frame.getContentPane().add(mainPanel);
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
